a27aa36ad0a337aaf183c9eef02bde2a3cce85be
Steven Rosario <strosariogarcia@gmail.com>
2022-11-16 14:17:34 -0500

    Convo Feature

--- client/src/Chatbox.js
+++ client/src/Chatbox.js
@@ -7,44 +7,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 function Chatbox() {
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState("");
-  const params = useParams();
-  console.log(params.id)
-  const cable = useRef();
 
-  // useEffect(() => {
-  //   if (!cable.current) {
-  //     cable.current = createConsumer("ws://localhost:3000/cable");
-  //   }
-
-  //   const paramsToSend = {
-  //     channel: "ConversationChannel",
-  //     id: params.id,
-  //   };
-  //   console.log(params)
-
-  //   const handlers = {
-  //     recevied(data) {
-  //       setMessages([...messages, data]);
-  //     },
-  //     connected() {
-  //       console.log("Connected");
-  //     },
-  //     disconnected() {
-  //       console.log("Disconnected");
-  //       cable.current = null;
-  //     },
-  //   };
-
-  //   const subscription = cable.subscriptions.create(paramsToSend, handlers);
-
-  //   return function cleanup() {
-  //     console.log("unsubbing from", params.id);
-  //     cable.current = null;
-  //     subscription.unsubscribe();
-  //   };
-  // }, [params.id, messages]);
 
   return (
     <div>
