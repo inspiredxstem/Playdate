0f94bfaed41a7251bf73ed3b88e2a99be4864a38
Steven Rosario <strosariogarcia@gmail.com>
2022-11-16 14:14:34 -0500

    Updated

--- client/src/PetCards.js
+++ client/src/PetCards.js
@@ -1,17 +1,38 @@
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
 
-function PetCards({ user}) {
-    return (
-      <div>
-        {/* <Navbar /> */}
+// Will probably change styling, I want the cards to be smaller and maybe be 4 across? idk
+
+function PetCards({ users }) {
+  return (
+    <div>
+      <div className="card">
         <div>
-          <h1>Pet Cards</h1>
-          <h2>{user.username}</h2>
+          <img className="profile-pic" src={users.profile_pic} />
         </div>
-        {/* <Footer /> */}
+        <div className="pet-info">
+          <div className="pet-title">
+            <div className="pet-name">
+              {users.name}, {users.age}
+            </div>
+            <div>
+              <Link className="chat-link" to="/inbox">
+                {/* Will change the chat link when I have the messaging components up and running */}
+                <i className="bx bxs-envelope"></i>
+              </Link>
+            </div>
+          </div>
+          {/* <div className="pet-details">{users.location}</div> */}
+          <div className="pet-details">New York, NY</div>
+          <div className="pet-bio">{users.bio}</div>
+          <div className="pet-gender">
+            <div className="pet-details">GENDER</div>
+            <div>{users.gender}</div>
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default PetCards;
+    </div>
+  );
+}
+
+export default PetCards;
