7f4d5b878a977c97d08215f7eff194bbc8f4901b
Steven Rosario <strosariogarcia@gmail.com>
2022-11-18 13:50:37 -0500

    Inbox Feature /Steven

--- client/src/App.js
+++ client/src/App.js
@@ -14,43 +14,80 @@ function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const [userInbox, setUserInbox] = useState([]);
   const [unreadMessages, setUnreadMessages] = useState([]);
-  
-  function handleCurrentUser(user) {
-    setCurrentUser(user)
-    setUserInbox([...user.get_conversations])
 
-    // const allUnreadMessages = []
-    // user?.get_conversations.forEach(conversation => {
-    //   conversation.convo?.unread_messages(message => {
-    //     if(message.user_id !== user.id){
-    //       allUnreadMessages.push(message)
-    //     }
-    //   })
-    // })
-    // setUnreadMessages([...allUnreadMessages])
-    console.log(user)
+  function handleCurrentUser(user) {
+    setCurrentUser(user);
+    setUserInbox([...user.get_conversations]);
+
+    const allUnreadMessages = [];
+    user.get_conversations.forEach((conversation) => {
+      conversation.unread_messages.forEach((message) => {
+        if (message.user_id !== user.id) {
+          allUnreadMessages.push(message);
+        }
+      });
+    });
+    setUnreadMessages([...allUnreadMessages]);
   }
 
-  useEffect(()=>{
-    if(localStorage.getItem('jwt')){  
+  useEffect(() => {
+    if (localStorage.getItem("jwt")) {
       axios
-        .get("http://localhost:3000/me", { headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}`}})
-        .then(res => {
-          setCurrentUser(res.data)
-          setUserInbox([...res.data.get_conversations])
+        .get("http://localhost:3000/me", {
+          headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` },
         })
-      }
-    },[])
-    
+        .then((res) => {
+          setCurrentUser(res.data);
+          setUserInbox([...res.data.get_conversations]);
+          const allUnreadMessages = [];
+          res.data.get_conversations.forEach((conversation) => {
+            conversation.unread_messages.forEach((message) => {
+              if (message.user_id !== res.data.id) {
+                allUnreadMessages.push(message);
+              }
+            });
+          });
+          setUnreadMessages([...allUnreadMessages]);
+        });
+    }
+  }, []);
 
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login handleCurrentUser={handleCurrentUser}/>} />
-        <Route path="/pets" element={<Pets currentUser={currentUser} userInbox={userInbox} setUserInbox={setUserInbox}/>} />
-        <Route path="/inbox" element={<Inbox />} />
-        <Route path="/inbox/:id" element={<Chatbox currentUser={currentUser} unreadMessages={unreadMessages} setUnreadMessages={setUnreadMessages}/>} />
+        <Route
+          path="/login"
+          element={<Login handleCurrentUser={handleCurrentUser} />}
+        />
+        <Route
+          path="/pets"
+          element={
+            <Pets
+              currentUser={currentUser}
+              userInbox={userInbox}
+              setUserInbox={setUserInbox}
+            />
+          }
+        />
+        <Route 
+          path="/inbox" 
+          element= {
+            <Inbox
+              userInbox={userInbox}
+              unreadMessages={unreadMessages} 
+            />
+          } 
+        />
+        <Route
+          path="/inbox/:id"
+          element={
+            <Chatbox
+              unreadMessages={unreadMessages}
+              setUnreadMessages={setUnreadMessages}
+            />
+          }
+        />
         {/* <Route path="/me" element={<Profile current={currentUser} />} /> */}
         <Route path="*" element={<NotFound />} />
       </Routes>
